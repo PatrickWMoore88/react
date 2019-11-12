6d0c7dd502368b03a45baa24173c306443fcc2ac
Patrick Moore <patrickwmoore88@gmail.com>
2019-11-12 10:40:11 -0600

    full stack react app with express

---
+++ repos/React/full-stack/client/src/components/Users.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+export class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users: [] };
+  }
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      // .then(users => this.setState({ users: users })) both are the same but the state key and value are the same name.
+      .then(users => this.setState({ users }));
+  }
+  render() {
+    return (
+      <ul>
+        {this.state.users.map(user => {
+          return <li>{user}</li>;
+        })}
+      </ul>
+    );
+  }
+}
+
+export default Users;
