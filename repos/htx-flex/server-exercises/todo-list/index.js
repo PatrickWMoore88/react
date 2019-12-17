4f1162368c9f9d4509bcf961249e92baf4900714
Patrick Moore <patrickwmoore88@gmail.com>
2019-12-17 10:59:29 -0600

    todo list WIP

--- repos/htx-flex/server-exercises/todo-list/index.js
+++ repos/htx-flex/server-exercises/todo-list/index.js
@@ -1,5 +1,17 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
+const promise = require("bluebird");
+
+const config = {
+  host: "localhost",
+  port: 5432,
+  database: "todo_list",
+  user: "postgres"
+};
+
+const initOptions = { promiseLib: promise };
+const pgp = require("pg-promise")(initOptions);
+const db = pgp(config);
 
 var app = express();
 
@@ -11,12 +23,26 @@ var counter = 1;
 var todoList = [];
 
 // GET /api/todos
-app.get("/api/todos", (req, res, next) => {
-  res.json(todoList);
+// app.get("/api/todos", (req, res, next) => {
+//   res.json(todoList);
+// });
+
+app.get("/todo", (req, res, next) => {
+  db.query("SELECT task FROM todo_items").then(function(result) {
+    // console.log(result);
+    res.json(result);
+  });
 });
 
 // GET /api/todos/:id
-app.get("/api/todos/:id", (req, res, next) => {
+// app.get("/api/todos/:id", (req, res, next) => {
+//   let todoItem = todoList.find(function(element) {
+//     return element.id.toString() === req.params.id;
+//   });
+//   res.json(todoItem);
+// });
+
+app.get("/todo/:id", (req, res, next) => {
   let todoItem = todoList.find(function(element) {
     return element.id.toString() === req.params.id;
   });
@@ -24,11 +50,20 @@ app.get("/api/todos/:id", (req, res, next) => {
 });
 
 // POST /api/todos
-app.post("/api/todos", (req, res, next) => {
-  counter++;
-  let todoItem = req.body;
-  (todoItem = { id: counter, ...todoItem }), todoList.push(todoItem);
-  res.json(todoList);
+// app.post("/api/todos", (req, res, next) => {
+//   counter++;
+//   let todoItem = req.body;
+//   (todoItem = { id: counter, ...todoItem }), todoList.push(todoItem);
+//   res.json(todoList);
+// });
+
+app.post("/todo", (req, res, next) => {
+  db.result(`INSERT INTO todo_items(task) VALUES ($1) RETURNING task`, [
+    req.body.todo
+  ]).then(function(result) {
+    console.log(result);
+    res.json(result.rows[0].task);
+  });
 });
 
 // PUT /api/todos/:id
